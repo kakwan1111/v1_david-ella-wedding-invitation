b45e264a1a840984586897f9022e8488792fa94e
kakwan1111 <100533160+kakwan1111@users.noreply.github.com>
2026-07-21 14:45:00 +0800

    Fix indentation in Home.tsx form structure

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -209,7 +209,7 @@ export default function Home() {
               <input type="text" id="guestName" name="guestName" required placeholder="Full Name" />
             </div>
 
-              <div className="form-row">
+              <div className="form-group">
                 <label htmlFor="guestPhone">Phone</label>
                 <input type="tel" id="guestPhone" name="guestPhone" placeholder="+852 XXXX XXXX" />
             </div>
